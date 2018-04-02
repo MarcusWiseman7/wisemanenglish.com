36487ad620f70a5853126464c7ad39d087a9abc4
Marcus <md.wiseman@hotmail.com>
2018-04-02 12:20:51 +0200

    Added favicon, pics, main js, content

---
+++ public/javascripts/main.js
@@ -0,0 +1,43 @@
+(($) => {
+  $(window).trigger('resize').trigger('scroll');
+
+  $('.parallax-window').parallax({
+    naturalWidth: 600,
+    naturalHeight: 400,
+  });
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+      let target = $(this.hash);
+      target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 70)
+        }, 1000, 'easeInOutExpo');
+        return false;
+      }
+    }
+  });
+
+  // Scroll to top button appear
+  $(document).scroll(function () {
+    const scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(() => {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 80,
+  });
+})(jQuery);
